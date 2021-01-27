24e88b5564b79c8f3dbb04d9b8c0650300f93b1a
EC2 Default User <ec2-user@ip-172-31-82-8.ec2.internal>
2021-01-27 07:05:15 +0000

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "public/homes#top"
   
-  namespace :admins do
+  namespace :public do
+    resources :end_users
   end
+  
 end
