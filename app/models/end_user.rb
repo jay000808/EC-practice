332b4de37135e254aa65a6a3f2bd21aec8f11b86
EC2 Default User <ec2-user@ip-172-31-82-8.ec2.internal>
2021-01-27 06:15:15 +0000

    first commit

---
+++ app/models/end_user.rb
@@ -0,0 +1,7 @@
+class EndUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :tel_number, presence: true
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :end_users
+  devise_for :admins
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "public/homes#top"
+  
+  namespace :admins do
+  end
+end
